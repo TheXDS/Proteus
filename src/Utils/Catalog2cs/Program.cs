417e48b09270252552ef1dac5ab3c634fe29204a
César Morgan <xds_xps_ivx@hotmail.com>
2020-02-06 15:23:38 -0600

    WIP: Tool de generaciónde seeder

---
+++ src/Utils/Catalog2cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using ExcelDataReader;
+using System;
+using System.IO;
+
+namespace Catalog2cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = File.Open(args[0], FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    do
+                    {
+                        while (reader.Read())
+                        {
+                            // reader.GetDouble(0);
+                        }
+                    } while (reader.NextResult());
+                }
+            }
+        }
+    }
+}
