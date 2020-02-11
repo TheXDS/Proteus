fd8ebd819e0fae1d08648f791e2f572e6d6060db
César Morgan <xds_xps_ivx@hotmail.com>
2020-02-10 18:02:05 -0600

    WIP: Updates de contabilidad

---
+++ src/DbServices/Contabilidad/Models/SubCuentaMolde.cs
@@ -0,0 +1,30 @@
+﻿using TheXDS.Proteus.Models.Base;
+
+namespace TheXDS.Proteus.Models
+{
+    public class SubCuentaMolde : Nameable<int>
+    {
+        public virtual CuentaMolde Parent { get; set; }
+
+        public virtual Divisa? DefaultDivisa { get; set; }
+
+        public static implicit operator SubCuenta(SubCuentaMolde molde)
+        {
+            return new SubCuenta
+            {
+                Name = molde.Name,
+                Divisa = molde.DefaultDivisa,
+                BalanceCache = 0m
+            };
+        }
+
+        public static implicit operator SubCuentaMolde(string name)
+        {
+            return new SubCuentaMolde
+            {
+                Name = name
+            };
+        }
+
+    }
+}
--- src/UiModules/ContabilidadUi/Crud/PeriodoDescriptor.cs
+++ src/UiModules/ContabilidadUi/Crud/PeriodoDescriptor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TheXDS.Proteus.ContabilidadUi.Modules;
 using TheXDS.Proteus.Crud.Base;
 using TheXDS.Proteus.Models;
 
@@ -15,6 +16,9 @@ namespace TheXDS.Proteus.ContabilidadUi.Crud
             DateProperty(p => p.Timestamp).Important("Fecha de inicio de periodo").Default(new DateTime(DateTime.Today.Year, 1, 1));
             DateProperty(p => p.Void).Nullable().Label("Fecha de cierre de periodo").AsListColumn().ShowInDetails();
             ListProperty(p => p.Partidas).Creatable().Label("Partidas del período").ShowInDetails().Required();
+            //AfterSave(ContabilidadModule.ModuleStatus.InitViewModel);
+            AfterSave(async () => await ContabilidadModule.ModuleStatus.InitViewModel());
+
         }
     }
 }
