afcff012b1e670789b19a3a3ca1320c9fdc150c9
César Morgan <xds_xps_ivx@hotmail.com>
2020-02-14 17:39:39 -0600

    Anotaciones de consultas

--- src/DbServices/Contabilidad/Models/CtaXPagar.cs
+++ src/DbServices/Contabilidad/Models/CtaXPagar.cs
@@ -12,6 +12,15 @@ namespace TheXDS.Proteus.Models
         public virtual Partida CreationPartida { get; set; }
         public virtual Partida? PaymentPartida { get; set; }
     }
+
+    /*
+     * Para consultar:
+     * Se puede pagar una cuenta desde caja chica? (debería ser posible)
+     * Es técnicamente posible pagar una cta.x.pag. con otros activos fijos?
+     * Debe el sistema imprimir cheques? (probablemente sí, en matricial)
+     * Se puede pagar una cta.x.pag. de forma parcial? o debe ser pagada por completo?
+     */
+
     public class CxpPayment : TimestampModel<long>
     {
         public virtual CtaXPagar Parent { get; set; }
