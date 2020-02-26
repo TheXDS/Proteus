7e9ba5f0682195c96beec45316c9f8a58ea3551d
César Morgan <xds_xps_ivx@hotmail.com>
2020-02-26 17:45:52 -0600

    WIP2: página de diag

--- src/UiModules/UiDemo/Pages/DiagnosticsPage.xaml.cs
+++ src/UiModules/UiDemo/Pages/DiagnosticsPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TheXDS.Proteus.Api;
 
 namespace TheXDS.Proteus.UiDemo.Pages
 {
@@ -22,6 +24,16 @@ namespace TheXDS.Proteus.UiDemo.Pages
         {
             InitializeComponent();
             //Title = "Información de diagnóstico de servicios";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Proteus.MessageTarget?.Info((LstServices.SelectedItem as Service)?.ChangesPending().ToString() ?? "Seleccione un servicio para continuar.");
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            await ((LstServices.SelectedItem as Service)?.ForcefullySaveAsync() ?? Task.FromResult(DetailedResult.Ok));
         }
     }
 }
