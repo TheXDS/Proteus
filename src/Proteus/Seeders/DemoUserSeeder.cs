cace7db09938cdd6960f3faddac447cd793b04d1
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2020-08-31 13:59:42 -0600

    Semillador de usuario demo

---
+++ src/Proteus/Seeders/DemoUserSeeder.cs
@@ -0,0 +1,39 @@
+﻿/*
+Copyright © 2017-2020 César Andrés Morgan
+Licenciado para uso interno solamente.
+*/
+
+using System.Collections.Generic;
+using TheXDS.MCART.Security.Password;
+using TheXDS.MCART.Types.Extensions;
+using TheXDS.Proteus.Api;
+using TheXDS.Proteus.Models;
+
+namespace TheXDS.Proteus.Seeders
+{
+    [SeederFor(typeof(UserService))]
+    public class DemoUserSeeder : AsyncDbSeeder<User>
+    {
+        protected override IEnumerable<User> GenerateEntities()
+        {
+            yield return new User
+            {
+                Id = "demo",
+                AllowMultiLogin = true,
+                ButtonBehavior = Models.Base.SecurityBehavior.Unlocked,
+                DefaultGranted = SecurityFlags.Root,
+                DefaultRevoked = SecurityFlags.None,
+                Descriptors = { },
+                Enabled = true,
+                Interactive = true,
+                IsDeleted = false,
+                ModuleBehavior = Models.Base.SecurityBehavior.Unlocked,
+                Name = "Usuario de demostración",
+                ScheduledPasswordChange = false,
+                Parent = null!,
+                Roles = { },
+                PasswordHash = PasswordStorage.CreateHash("demo".ToSecureString())
+            };
+        }
+    }
+}
