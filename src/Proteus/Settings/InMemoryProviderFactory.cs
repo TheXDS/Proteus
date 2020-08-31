40c67e31da3526128905dc97723735b7986563a3
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2020-08-31 12:10:07 -0600

    WIP: Proveedor InMemory

---
+++ src/Proteus/Settings/InMemoryProviderFactory.cs
@@ -0,0 +1,19 @@
+﻿/*
+Copyright © 2017-2020 César Andrés Morgan
+Licenciado para uso interno solamente.
+*/
+
+using System;
+using System.Data.Common;
+using System.Data.Entity.Infrastructure;
+
+namespace TheXDS.Proteus.Component
+{
+    public class InMemoryProviderFactory : IDbConnectionFactory
+    {
+        public DbConnection CreateConnection(string nameOrConnectionString)
+        {
+            return Effort.DbConnectionFactory.CreatePersistent($"Proteus_{Guid.NewGuid()}");
+        }
+    }
+}
